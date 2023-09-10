5365ae375b63049e3709422f8e4d3c7553987b1f
Ben C <greenboyb1@gmail.com>
2023-09-10 02:34:56 -0400

    added rest of chap 1 hw

---
+++ Chapter 1/1.22 Hello World!/main.cpp
@@ -0,0 +1,15 @@
+/*
+Write a program that outputs "Hello World!" as shown below. For ALL labs, end with newline (unless otherwise stated).
+
+    *Hello World!
+
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    //Prints Hello World!
+    cout << "Hello World!" << endl;
+    return 0;
+}
---
+++ Chapter 1/1.27 Basic output with variables/main.cpp
@@ -0,0 +1,52 @@
+/*
+This zyLab activity prepares a student for a full programming assignment. Warm up exercises are typically simpler and worth fewer points than a full programming assignment, and are well-suited for an in-person scheduled lab meeting or as self-practice.
+
+
+A variable like userNum can store a value like an integer. Extend the given program as indicated.
+
+    1. Output the user's input. (2 pts)
+    2. Output the input squared and cubed. Hint: Compute squared as userNum * userNum. (2 pts)
+    3. Get a second user input into userNum2, and output the sum and product. (1 pt)
+
+Note: This zyLab outputs a newline after each user-input prompt. For convenience in the examples below, the user's input value is shown on the next line, but such values don't actually appear as output when the program runs. (User Input in Bold)
+
+    *Enter integer:
+    *<b>4</b>
+    *You entered: 4
+    *4 squared is 16 
+    *And 4 cubed is 64!!
+    *Enter another integer:
+    *<b>5</b>
+    *4 + 5 is 9
+    *4 * 5 is 20
+
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    //Initializing Variables
+    int userNum0;
+    int userNum1;
+
+    //Getting user's first number
+    cout << "Enter integer:" << endl;
+    cin  >> userNum0;
+
+    //Printing user's first number
+    cout << "You entered: " << userNum0 << endl;
+
+    //Printing user's first number squared and cubed
+    cout << userNum0 << " squared is " << userNum0 * userNum0 << endl;
+    cout << "And " << userNum0 << " cubed is " << userNum0 * userNum0 * userNum0 << "!!" << endl;
+
+    //Getting user's second number
+    cout << "Enter another integer:" << endl;
+    cin  >> userNum1;
+
+    //Printing user's first and second number added and multiplied together
+    cout << userNum0 << " + " << userNum1 << " is " << userNum0 + userNum1 <<  endl;
+    cout << userNum0 << " * " << userNum1 << " is " << userNum0 * userNum1 << endl;
+    return 0;
+}
