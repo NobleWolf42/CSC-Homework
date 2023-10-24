f65ee524cf087036585b1c519ccf239a18f14aa2
Ben C. <greenboyb1@gmail.com>
2023-10-24 11:45:05 -0400

    some ch5 hw

---
+++ Chapter 5/5.29 Track laps to miles/main.cpp
@@ -0,0 +1,46 @@
+/*
+One lap around a standard high-school running track is exactly 0.25 miles. Define a function named LapsToMiles that takes a double as a parameter, representing the number of laps, and returns a double that represents the number of miles. Then, write a main program that takes a number of laps as an input, calls function LapsToMiles() to calculate the number of miles, and outputs the number of miles.
+
+Output each floating-point value with two digits after the decimal point, which can be achieved by executing
+cout << fixed << setprecision(2); once before all other cout statements.
+
+Ex: If the input is:
+
+    *7.6
+
+the output is:
+
+    *1.90
+
+Ex: If the input is:
+
+    *2.2
+
+the output is:
+
+    *0.55
+
+The program must define and call a function:
+double LapsToMiles(double userLaps)
+*/
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+double LapsToMiles(double userLaps) {
+    return userLaps * 0.25;
+}
+
+int main() {
+    
+    //Declaring variables
+    double laps;
+
+    //Getting user input
+    cin >> laps;
+
+    //Outputting results
+    cout << fixed << setprecision(2) << LapsToMiles(laps) << endl;
+
+    return 0;
+}
