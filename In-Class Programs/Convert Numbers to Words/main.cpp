0d7331231bd91a8640b95e82d166f9a33ee73d88
Ben C. <greenboyb1@gmail.com>
2023-11-07 10:45:07 -0500

    adding more testing

--- In-Class Programs/Convert Numbers to Words/main.cpp
+++ In-Class Programs/Convert Numbers to Words/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <cassert>
+
+void UserInputOutput();
+
 using namespace std;
+
+//string &getString(size_t num, size_t count, string &numstr, int tdigit);
 
 string spellDigit(size_t d) {
     string numstr = "";
@@ -11,7 +17,7 @@ string spellDigit(size_t d) {
             numstr = "one";
             break;
         case 2:
-            numstr =  "two";
+            numstr = "two";
             break;
         case 3:
             numstr = "three";
@@ -54,7 +60,7 @@ string spellTenth(size_t d2, size_t d1) {
                     numstr = "eleven";
                     break;
                 case 2:
-                    numstr =  "twelve";
+                    numstr = "twelve";
                     break;
                 case 3:
                     numstr = "thirteen";
@@ -83,7 +89,7 @@ string spellTenth(size_t d2, size_t d1) {
 
             break;
         case 2:
-            numstr =  "twenty";
+            numstr = "twenty";
             if (d1 > 0) {
                 numstr = "twenty";
                 numstr = numstr + " " + spellDigit(d1);
@@ -145,40 +151,114 @@ string spellTenth(size_t d2, size_t d1) {
     return numstr;
 }
 
-string spellThree(int digits) {
-    
-}
-
-int main() {
-    std::cout << "Enter a number:" << std::endl;
-    size_t num;
-    cin >> num;
-
+string spell3Digits(int num) {
     size_t count = 0;
     string numstr = "";
     size_t d0;
     while (num > 0) {
         size_t digit = num % 10;
-        cout << digit << endl;
+        // cout << digit << endl;
+
         if (count == 0) {
             numstr = spellDigit(digit);
-        }
-        else if (count == 1){
+        } else if (count == 1) {
             numstr = spellTenth(digit, d0);
-        }
-        else if (count == 2) {
-            if (digit > 0 && numstr == "zero") {
-                numstr = spellDigit(digit) + " hundred";
-            } else if (digit > 0) {
-                numstr = spellDigit(digit) + " hundred " + numstr;
-            }
+        } else if (count == 2) {
+            if (digit > 0) {
+                if (numstr == "zero") {
+                    numstr = spellDigit(digit) + " hundred ";
+                } else {
+                    numstr = spellDigit(digit) + " hundred " + numstr;
+                }
 
+            }
         }
-        cout << numstr << " " << count << endl;
+
+        // cout << numstr << " " << count << endl;
         count++;
         d0 = digit;
         num = num / 10;  // get rid of one digit
-
     }
+    return numstr;
+}
+
+string &getString(size_t num, size_t count, string &numstr, int tdigit) {
+    switch (count) {
+        case 0:
+            numstr = spell3Digits(num % 1000);
+            break;
+        case 1:
+            tdigit = num % 1000;
+            if (tdigit > 0) {
+                numstr = spell3Digits(num % 1000) + " thousand " + numstr;
+            }
+            break;
+        case 2:
+            tdigit = num % 1000;
+            if (tdigit > 0) {
+                numstr = spell3Digits(num % 1000) + " million " + numstr;
+            }
+            break;
+        case 3:
+            tdigit = num % 1000;
+            if (tdigit > 0) {
+                numstr = spell3Digits(num % 1000) + " billion " + numstr;
+            }
+
+            break;
+    }
+    return numstr;
+}
+
+int main() {
+    UserInputOutput();
+
+
+    assert(spellDigit(0) == "zero");
+    assert(spellDigit(1) == "one");
+    assert(spellDigit(2) == "two");
+    assert(spellDigit(3) == "three");
+    assert(spellDigit(4) == "four");
+    assert(spellDigit(5) == "five");
+    assert(spellDigit(6) == "six");
+    assert(spellDigit(7) == "seven");
+    assert(spellDigit(8) == "eight");
+    assert(spellDigit(9) == "nine");
+
+    assert(spellTenth(1, 0) == "ten");
+    assert(spellTenth(1, 1) == "eleven");
+    assert(spellTenth(1, 2) == "twelve");
+    assert(spellTenth(1, 3) == "thirteen");
+    assert(spellTenth(1, 4) == "fourteen");
+    assert(spellTenth(1, 5) == "fifteen");
+    assert(spellTenth(1, 6) == "sixteen");
+    assert(spellTenth(1, 7) == "seventeen");
+    assert(spellTenth(1, 8) == "eighteen");
+    assert(spellTenth(1, 9) == "nineteen");
+
+    assert(spellTenth(2, 0) == "twenty");
+    assert(spellTenth(3, 0) == "thirty");
+    assert(spellTenth(4, 0) == "forty");
+    assert(spellTenth(5, 0) == "fifty");
+    assert(spellTenth(6, 0) == "sixty");
+    assert(spellTenth(7, 0) == "seventy");
+    assert(spellTenth(8, 0) == "eighty");
+    assert(spellTenth(9, 0) == "ninety");
+
     return 0;
+}
+
+void UserInputOutput() {
+    cout << "Enter a number:" << endl;
+    size_t num;
+    cin >> num;
+    size_t count = 0;
+    string numstr = "";
+    int tdigit = 0;
+    while (num > 0) {
+        numstr = getString(num, count, numstr, tdigit);
+        count++;
+        num = num / 1000;
+    }
+    cout << numstr << endl;
 }
