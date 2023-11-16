f45c84ef870b0d24931cc6c21c3fb44ab8b360a7
Ben C. <greenboyb1@gmail.com>
2023-11-16 10:32:08 -0500

    notes i guess

--- Chapter 5/CH05 EXAM/main.cpp
+++ Chapter 5/CH05 EXAM/main.cpp
@@ -1,6 +1,18 @@
+/*
+use ASCII char codes to replace message in order to encode it
+*/
+
 #include <iostream>
 
 using namespace std;
+
+void encoder(string& message, int shift) {
+
+}
+
+void decoder(string& message, int shift) {
+
+}
 
 
 int main () {
