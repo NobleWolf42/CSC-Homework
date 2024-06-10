437e384f058d42501f64e19387535b642f501c07
NobleWolf42 <greenboyb1@gmail.com>
2024-06-10 13:01:34 -0400

    might be fully working

--- CSC 211-B1/CH07Test - River System Recursion/Rivers.h
+++ CSC 211-B1/CH07Test - River System Recursion/Rivers.h
@@ -74,53 +74,6 @@ public:
     }
 
     /**
-     * any-unhealthy?:  River -> Boolean
-     * consumes a river system and returns true if any river in the system has a pH less than 6.5 or greater than 8.5,
-     * @return true or false
-     */
-    bool unhealthy() {
-        return unhealthy_recursive(this);
-    }
-
-    /**
-     * lower-all-ph:  River -> River
-     * consumes a river system and produces a river system the same as the original,
-     * except all pH readings are 0.1 lower
-     * @return Rivers*
-     */
-    Rivers* lower_all_ph() {
-        lower_all_ph_recursive(this);
-        return this;
-    }
-
-    /**
-     * find-subsystem:  River String-> River OR #false
-     * consumes a river system and the name of the river.  The function returns that
-     * part of the system that has the named river as its root (or returns false
-     * if there is no river in the system with the given name)
-     * @param a
-     * @return river or NULL
-     */
-    Rivers* find_subsystem(string name) {
-        if (this->getRiverName() == name) {
-            return this;
-        }
-        return find_subsystem_recursive(rivers, name);
-    }
-
-
-    /**
-     * print out the rivers with indented (4 space) for the sub-rivers.
-     * @param rivers
-     * @param level
-     */
-    void print() {
-        print(this, 0);
-    }
-
-
-
-    /**
      * checks a river system with an acidic river, and also with a river considered non-acidic
      * (on the boundary at acid_level.  The default is 7.0)
      * the acidic river is listed, the river with pH=7.0 is not
@@ -146,20 +99,27 @@ public:
 
     /**
      * any-unhealthy?:  River -> Boolean
+     * consumes a river system and returns true if any river in the system has a pH less than 6.5 or greater than 8.5,
+     * @return true or false
+     */
+    bool unhealthy() {
+        return unhealthy(this);
+    }
+
+    /**
+     * any-unhealthy?:  River -> Boolean
      * consumes a river and produces true if the river's ph is under 6.5 or over 8.5,
      * or if the DO is less than 6ppm
      * @return true or false
      */
     bool unhealthy(Rivers* rivers) {
-        if (rivers->getPh() < 6.5 || rivers->getPh() > 8.5) {
+        if (bad_numbers(rivers)) {
             return true;
+        } else if (unhealthy_trib(this->getRivers().begin(), this->getRivers().end())) {
+            return true;
+        } else {
+            return false;
         }
-        for (Rivers* subRiver : rivers->getRivers()) {
-            if (unhealthy(subRiver)) {
-                return true;
-            }
-        }
-        return false;
     }
 
     bool unhealthy_trib(list<Rivers*>::const_iterator begin, list<Rivers*>::const_iterator end) {
@@ -167,10 +127,13 @@ public:
             return false;
         }
         Rivers* river = *begin;
-        if (river->getPh() < 6.5 || river->getPh() > 8.5) {
+        if (bad_numbers(river)) {
             return true;
+        } else if (unhealthy_trib(++begin, end) || unhealthy_trib(river->getRivers().begin(), river->getRivers().end())) {
+            return true;
+        } else {
+            return false;
         }
-        return unhealthy_trib(++begin, end);
     }
 
     /**
@@ -182,13 +145,20 @@ public:
     bool bad_numbers(Rivers* river) {
         if (river->getPh() < 6.5 || river->getPh() > 8.5 || river->getDo() < 6) {
             return true;
+        } else {
+            return false;
         }
-        for (Rivers* subRiver : river->getRivers()) {
-            if (bad_numbers(subRiver)) {
-                return true;
-            }
-        }
-        return false;
+    }
+
+    /**
+     * lower-all-ph:  River -> River
+     * consumes a river system and produces a river system the same as the original,
+     * except all pH readings are 0.1 lower
+     * @return Rivers*
+     */
+    Rivers* lower_all_ph() {
+        Rivers* newRivers = new Rivers();
+        return lower_all_ph(this, newRivers);
     }
 
     /**
@@ -198,11 +168,9 @@ public:
      * @return Rivers*
      */
     Rivers* lower_all_ph(Rivers* rivers, Rivers* newRivers) {
-        rivers->setPh(rivers->getPh() - 0.1);
-        for (Rivers* subRiver : rivers->getRivers()) {
-            lower_all_ph(subRiver, newRivers);
-        }
-        return rivers;
+        list<Rivers*> temp;
+        list<Rivers*> nR = update_ph(rivers->getRivers().begin(), rivers->getRivers().end(), temp);
+        return new Rivers(rivers->getRiverName(), rivers->getPh() - .1, rivers->getDo(), nR);
     }
 
     list<Rivers*> update_ph(list<Rivers*>::const_iterator begin, list<Rivers*>::const_iterator end, list<Rivers*> newRivers) {
@@ -211,10 +179,26 @@ public:
         }
         Rivers* river = *begin;
         river->setPh(river->getPh() - 0.1);
+        if (!river->getRivers().empty()) {
+            list<Rivers*> temp;
+            list<Rivers*> nR = update_ph(river->getRivers().begin(), river->getRivers().end(), temp);
+            river->setRivers(nR);
+        }
         newRivers.push_back(river);
         return update_ph(++begin, end, newRivers);
     }
 
+    /**
+     * find-subsystem:  River String-> River OR #false
+     * consumes a river system and the name of the river.  The function returns that
+     * part of the system that has the named river as its root (or returns false
+     * if there is no river in the system with the given name)
+     * @param a
+     * @return river or NULL
+     */
+    Rivers* find_subsystem(string name) {
+        return find_subsystem(this, name);
+    }
 
     /**
      * find-subsystem:  River String-> River OR #false
@@ -228,13 +212,8 @@ public:
         if (rivers->getRiverName() == name) {
             return rivers;
         }
-        for (Rivers* subRiver : rivers->getRivers()) {
-            Rivers* result = find_subsystem(subRiver, name);
-            if (result != nullptr) {
-                return result;
-            }
-        }
-        return nullptr;
+        Rivers* out = find_in_list(rivers->getRivers().begin(), rivers->getRivers().end(), name);
+        return out;
     }
 
     /**
@@ -248,13 +227,27 @@ public:
      */
     Rivers* find_in_list(list<Rivers*>::const_iterator begin, list<Rivers*>::const_iterator end, string name) {
         if (begin == end) {
-            return nullptr;
+            return NULL;
         }
         Rivers* river = *begin;
         if (river->getRiverName() == name) {
             return river;
+        } else if (find_in_list(++begin, end, name) != NULL) {
+            return find_in_list(begin, end, name);
+        } else if (find_in_list(river->getRivers().begin(), river->getRivers().end(), name) != NULL) {
+            return find_in_list(river->getRivers().begin(), river->getRivers().end(), name);
+        } else {
+            return NULL;
         }
-        return find_in_list(++begin, end, name);
+    }
+
+    /**
+     * print out the rivers with indented (4 space) for the sub-rivers.
+     * @param rivers
+     * @param level
+     */
+    void print() {
+        print(this);
     }
 
     /**
@@ -263,12 +256,13 @@ public:
      * @param level
      */
     void print(Rivers* rivers, int level=0) {
-        for (int i = 0; i < level; i++) {
-            std::cout << "    ";
+        if (rivers == NULL) {
+            return;
         }
+        printSpaces(0, level);
         std::cout << rivers->getRiverName() << std::endl;
-        for (Rivers* subRiver : rivers->getRivers()) {
-            print(subRiver, level + 1);
+        if (!rivers->getRivers().empty()) {
+            print(rivers->getRivers().begin(), rivers->getRivers().end(), level + 1);
         }
     }
 
@@ -276,17 +270,19 @@ public:
         if (begin == end) {
             return;
         }
+        printSpaces(0, level);
         Rivers* river = *begin;
-        for (int i = 0; i < level; i++) {
-            std::cout << "    ";
-        }
         std::cout << river->getRiverName() << std::endl;
         print(river->getRivers().begin(), river->getRivers().end(), level + 1);
         print(++begin, end, level);
     }
 
-    void static setAcidLevel(double acidLevel) {
-        acid_level = acidLevel;
+    void printSpaces(int start, int level) {
+        std::cout << "    ";
+        if (start == level) {
+            return;
+        }
+        printSpaces(start + 1, level);
     }
     
 private:
@@ -294,51 +290,6 @@ private:
     double pH;
     double DO;
     list<Rivers*> rivers;
-    static double acid_level;
-
-    bool unhealthy_recursive(Rivers* rivers) {
-        if (rivers->getPh() < 6.5 || rivers->getPh() > 8.5) {
-            return true;
-        }
-        for (auto river : rivers->getRivers()) {
-            if (unhealthy_recursive(river)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void lower_all_ph_recursive(Rivers* rivers) {
-        rivers->setPh(rivers->getPh() - 0.1);
-        for (auto river : rivers->getRivers()) {
-            lower_all_ph_recursive(river);
-        }
-    }
-
-    Rivers* find_subsystem_recursive(Rivers* rivers, string name) {
-        for (auto river : rivers->getRivers()) {
-            if (river->getRiverName() == name) {
-                return river;
-            }
-            Rivers* result = find_subsystem_recursive(river, name);
-            if (result != nullptr) {
-                return result;
-            }
-        }
-        return nullptr;
-    }
-
-    void print_recursive(list<Rivers*>::const_iterator begin, list<Rivers*>::const_iterator end, int level) {
-        if (begin == end) {
-            return;
-        }
-        for (int i = 0; i < level; ++i) {
-            std::cout << "    ";
-        }
-        std::cout << (*begin)->getRiverName() << std::endl;
-        print_recursive((*begin)->getRivers().begin(), (*begin)->getRivers().end(), level + 1);
-        print_recursive(++begin, end, level);
-    }
 
 };
 
--- CSC 211-B1/CH07Test - River System Recursion/main.cpp
+++ CSC 211-B1/CH07Test - River System Recursion/main.cpp
@@ -7,51 +7,51 @@ Copyright: 2024
 #include "Rivers.h"
 using namespace std;
 
-//int main() {
-//    list<Rivers> rs1;
-//    Rivers r1("Missouri",  6.5,  6.5, rs1);
-//    list<Rivers> rs2;
-//    Rivers r2("Jefferson", 7.0, 6.2, rs2);
-//    r1.make_river(r2);
-//    list<Rivers> rs3;
-//    Rivers r3("Beaverhead",  6.2, 6.7, rs3);
-//    r2.make_river(r3);
-//
-//    list<Rivers> rs4;
-//    Rivers r4("Bighole",  7.0, 7.5, rs4);
-//    r2.make_river(r4);
-//
-//    list<Rivers> rs5;
-//    Rivers r5("Sun", 7.5, 7.0, rs5);
-//    r1.make_river(r5);
-//
-//    list<Rivers> rs6;
-//    Rivers r6("Yellowstone", 7.0, 7.5, rs6);
-//    r1.make_river(r6);
-//
-//    list<Rivers> rs7;
-//    Rivers r7("Gardner", 7.1, 6.9, rs7);
-//    r6.make_river(r7);
-//
-//    list<Rivers> rs8;
-//    Rivers r8("Shields", 7.9, 8.0, rs8);
-//    r6.make_river(r8);
-//
-//    list<Rivers> rs9;
-//    Rivers r9("Boulder", 8.5, 5.5, rs9);
-//    r6.make_river(r9);
-//
-//    list<Rivers> rs10;
-//    Rivers r10("Madison", 8.0, 8.0, rs10);
-//    r1.make_river(r10);
-//
-//    list<Rivers> rs11;
-//    Rivers r11("Gallatin", 6.8, 7.2, rs11);
-//    r1.make_river(r11);
-//
-//    cout << r2.list_acidic_rivers() << endl;
-//    return 0;
-//}
+/*int main() {
+    list<Rivers> rs1;
+    Rivers r1("Missouri",  6.5,  6.5, rs1);
+    list<Rivers> rs2;
+    Rivers r2("Jefferson", 7.0, 6.2, rs2);
+    r1.make_river(r2);
+    list<Rivers> rs3;
+    Rivers r3("Beaverhead",  6.2, 6.7, rs3);
+    r2.make_river(r3);
+
+    list<Rivers> rs4;
+    Rivers r4("Bighole",  7.0, 7.5, rs4);
+    r2.make_river(r4);
+
+    list<Rivers> rs5;
+    Rivers r5("Sun", 7.5, 7.0, rs5);
+    r1.make_river(r5);
+
+    list<Rivers> rs6;
+    Rivers r6("Yellowstone", 7.0, 7.5, rs6);
+    r1.make_river(r6);
+
+    list<Rivers> rs7;
+    Rivers r7("Gardner", 7.1, 6.9, rs7);
+    r6.make_river(r7);
+
+    list<Rivers> rs8;
+    Rivers r8("Shields", 7.9, 8.0, rs8);
+    r6.make_river(r8);
+
+    list<Rivers> rs9;
+    Rivers r9("Boulder", 8.5, 5.5, rs9);
+    r6.make_river(r9);
+
+    list<Rivers> rs10;
+    Rivers r10("Madison", 8.0, 8.0, rs10);
+    r1.make_river(r10);
+
+    list<Rivers> rs11;
+    Rivers r11("Gallatin", 6.8, 7.2, rs11);
+    r1.make_river(r11);
+
+    cout << r2.list_acidic_rivers() << endl;
+    return 0;
+}*/
 
 
 int main() {
@@ -96,22 +96,21 @@ int main() {
     Rivers* r11 = new Rivers("Gallatin", 6.8, 7.2, rs11);
     r1->make_river(r11);
 
-//    cout << r2->list_acidic_rivers() << endl;
-    Rivers::setAcidLevel(6.5);
+    cout << r2->list_acidic_rivers() << endl;
     cout << r1->list_acidic_rivers() << endl;
 
-//    cout << r5->unhealthy() << endl;
+    cout << r1->unhealthy() << endl;
 
-//    r1->print();
-//    Rivers* r12 = r1->lower_all_ph();
-//    r12->print();
+    //r1->print();
+    Rivers* r12 = r1->lower_all_ph();
+    //r12->print();
 
 
-//    Rivers* r13 = r1->find_subsystem("Yellowstone");
-//    r13->print();
+    Rivers* r13 = r1->find_subsystem("Yellowstone");
+    r13->print();
 
-//    Rivers* r13 = r1->find_subsystem("Assabet");
-//    r13->print();
-//    r1->print();
+    Rivers* r14 = r1->find_subsystem("Assabet");
+    r14->print();
+    //r1->print();
     return 0;
 }
